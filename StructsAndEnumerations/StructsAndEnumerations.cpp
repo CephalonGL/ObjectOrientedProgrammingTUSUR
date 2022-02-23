3cde624bbcdba09bd6eb46cd62d9294e6d7cf392
Gregorii Lubov <glubov@yandex.ru>
2022-02-23 21:06:01 +0700

    feature(DemoSort): add method DemoSort()

--- StructsAndEnumerations/StructsAndEnumerations.cpp
+++ StructsAndEnumerations/StructsAndEnumerations.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <exception>
+
+using namespace std;
+
+void Sort(double* values, int count)
+{
+	if (count < 0)
+	{
+		throw exception("Fatality. Count < 0.");
+	}
+	double swap;
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < count; j++)
+		{
+			if (values[i] < values[j])
+			{
+				swap = values[i];
+				values[i] = values[j];
+				values[j] = swap;
+			}
+		}
+	}
+}
+
+void DemoSort(double arrayUnit[], int count)
+{
+	cout << endl << "-------------------------------------" << endl;
+	try
+	{
+		Sort(arrayUnit, count);
+	}
+	catch (const exception& error)
+	{
+		cout << "Exception catched!" << endl;
+		cout << "Error: " << error.what() << endl;
+		return;
+	}
+	cout << "Sort successful! Count = " << count << '.' << endl;
+	for (size_t i = 0; i < count; i++)
+	{
+		cout << arrayUnit[i] << '\t';
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int countArrayElements = 5;
+
+	double* valuesFirst = new double[countArrayElements] {100.0, 249.0, 12.0, 45.0, 23.5};
+	DemoSort(valuesFirst, 5);
+	
+	double* valuesSecond = new double[countArrayElements] {100.0, 249.0, 12.0, 45.0, 23.5};
+	DemoSort(valuesSecond, -1);
+	
+	double* valuesThird = new double[countArrayElements] {100.0, 249.0, 12.0, 45.0, 23.5};
+	DemoSort(valuesThird, 0);
+
+	delete[] valuesFirst;
+	delete[] valuesSecond;
+	delete[] valuesThird;
+
+	return 0;
 }
