ab8e94ed198e5702c9f90a1937e6e5fb2de320fc
Gregorii Lubov <glubov@yandex.ru>
2022-04-02 00:23:18 +0700

    style(LAB3-5): uncommented lines

--- LAB3-5/Main.cpp
+++ LAB3-5/Main.cpp
@@ -3,23 +3,23 @@
 int main()
 {
 	//Lab 3-4:
-	//DemoBook();
-	//Flight::DemoFlightWithTime();
-	//Band::DemoBand();
-	//GeometricProgram::DemoRectangleWithPoint();
-	//GeometricProgram::DemoRing();
-	//GeometricProgram::DemoCollision();
-	//
-	////Lab 5:
-	//Person person = Person("Ivan", "Ivanov", "Ivanovich");
-	//Student student = Student("Dmitry", "Dmitriev", "Dmitrievich", 1, 2022);
-	//Teacher teacher = Teacher("Anton", "Antonov", "Antonovich", Dean);
-	//
-	//ShowPerson::DemoName(&person);
-	//ShowPerson::DemoName(&student);
-	//ShowPerson::DemoName(&teacher);
-	//
-	//UserInterface::DemoUsers();
+	DemoBook();
+	Flight::DemoFlightWithTime();
+	Band::DemoBand();
+	GeometricProgram::DemoRectangleWithPoint();
+	GeometricProgram::DemoRing();
+	GeometricProgram::DemoCollision();
+	
+	//Lab 5:
+	Person person = Person("Ivan", "Ivanov", "Ivanovich");
+	Student student = Student("Dmitry", "Dmitriev", "Dmitrievich", 1, 2022);
+	Teacher teacher = Teacher("Anton", "Antonov", "Antonovich", Dean);
+	
+	ShowPerson::DemoName(&person);
+	ShowPerson::DemoName(&student);
+	ShowPerson::DemoName(&teacher);
+	
+	UserInterface::DemoUsers();
 
 	DiscountBase::DemoDiscount();
 	return 0;
