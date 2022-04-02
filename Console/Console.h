e192413ec17f83d9dd33993347cc5875334a7558
Gregorii Lubov <glubov@yandex.ru>
2022-04-02 21:30:34 +0700

    style(Console): add XML

--- Console/Console.h
+++ Console/Console.h
@@ -12,13 +12,24 @@ public:
 	/// Print message if got, then read
 	/// value from console and check if it is int.
 	/// </summary>
-	/// <param name="message">Message to print 
-	/// before reading</param>
+	/// <param name="message">Message to print before reading</param>
 	/// <returns>Int input value from console</returns>
 	static int ReadInt(const string& message = "");
 
+	/// <summary>
+	/// Print message if got, then read
+	/// value from console and check if it is unsigned int.
+	/// </summary>
+	/// <param name="message">Message to print before reading</param>
+	/// <returns>Unsigned int input value from console</returns>
 	static unsigned int ReadUnsignedInt(const string& message = "");
 
+	/// <summary>
+	/// Print message if got, then read
+	/// value from console and check if it is float.
+	/// </summary>
+	/// <param name="message">Message to print before reading</param>
+	/// <returns>Float input value from console</returns>
 	static float ReadFloat(const string& message = "");
 
 	/// <summary>
@@ -33,5 +44,5 @@ public:
 	/// Print message to console
 	/// </summary>
 	/// <param name="message">Message to print</param>
-	static void WriteLine(const string& message);
+	static void WriteLine(const string& message = "");
 };
